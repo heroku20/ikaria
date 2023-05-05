d5ff7370c2f02c603a303cc4bfc972026dcae23c
heroku20 <130670798+heroku20@users.noreply.github.com>
2023-05-05 18:22:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,232 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Ikaria Lean Belly Juice™</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="title" content="Where Can I buy Ikaria Lean Belly Juice
+      In UK Australia or USA | ikaria lean belly juice buy ">
+    <meta name="description" content="Ikaria Lean Belly Juice Buy it
+      here. Ikaria Lean Belly Juice isn\'t sold on Amazon or elsewhere.
+      The business advises ordering from the official website and not
+      trusting sellers.">
+    <meta name="keywords" content="where can i buy ikaria lean belly
+      juice">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="language" content="English">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initail-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="images/shkfrgChKCY9.png">
+    <link href=\'https://d33wubrfki0l68.cloudfront.net/css/294373a86bfcac9f778167a9c496bf7007118c42/css/vbthw7zw7lzk.css\' rel=\'stylesheet\' />
+
+
+    <meta name="google-site-verification" content="gZpwUgNnpow2tnYr0udOgYTkuXK-Fet5D9ch_i82jSY" />
+</head>
+
+<body>
+    <div class="wrap" align="center">
+        <div class="main-cont ">
+            <h1 class="brand al-cnter">Ikaria Lean™ Official | Where Can I Buy Ikaria Lean belly Juice<br>
+            </h1>
+            <div href="https://8mekb.bemobtrcks.com/go/d370498b-af01-4e76-91e6-3e60e08d5173">
+                <div><img src="https://d33wubrfki0l68.cloudfront.net/0d452551c4c421c3491489824e8f5db6b8006283/bbf86/images/ikaria-lean-official-where-can-i-buy-ikaria-lean-belly-juice-uk-australia-usa.png" class="main-img" alt="Ikaria-Lean-Official-Where-Can-I-Buy-Ikaria-Lean-belly-Juice-UK-Australia-USA"
+                        moz-do-not-send="true" width="600" height="413"> <a href="https://8mekb.bemobtrcks.com/go/d370498b-af01-4e76-91e6-3e60e08d5173"><img
+                src="https://d33wubrfki0l68.cloudfront.net/f4981ae7063dd63a36956d8258de0b9f7839361d/de9f6/images/3nhqxlolqzxj.gif" class="main-img"
+                alt="Order-now"></a><br>
+                </div>
+                <a href="https://8mekb.bemobtrcks.com/go/d370498b-af01-4e76-91e6-3e60e08d5173"><img
+              src="https://d33wubrfki0l68.cloudfront.net/ba563c0d5c2298210c6963947b52249a4d2a0309/6373c/images/bmewrufsugzr.png" class="main-img-g mbt-1"
+              alt="Gaurantee"></a> </div>
+            <div class="cont-e">
+                <h2 class="mn-hed al-cnter">What is Ikaria Lean Belly Juice?</h2>
+                <p> The Ikaria Lean Belly Juice is a potent blend of exotic nutrients specifically developed to rid your body of the newly discovered root cause of stubborn fat accumulation… destructive ceramide compounds.</p>
+                <p>The Ikaria Lean Belly Juice flushes out ceramides and renews your body from within…</p>
+                <p>Allowing you to dissolve stubborn, clogged fat easily and flood your body with newfound energy and vitality,</p>
+  
+                </p>
+                <p>Introducing...The Ikaria Lean Belly Juice!</p>
+                <h2 class="mn-hed al-cnter">Ikaria Lean Belly Juice Ingredients
+                </h2>
+                <p>The Ikaria Lean Belly Juice weight loss formula carefully combines a host of scientifically proven ingredients to target the decrease of uric acid in your body. A metabolic blend of 8 of the most powerful, exotic fat-burning nutrients
+                    on the planet.</p>
+                <div class="mant-sec al-cnter"> <a href="https://8mekb.bemobtrcks.com/go/d370498b-af01-4e76-91e6-3e60e08d5173"><img
+                src="https://d33wubrfki0l68.cloudfront.net/a35489edb71a4ed05d254194f5fb526b8cf31e8b/a2df7/images/gqnyah5jrbzv.webp" class="main-img-i"
+                alt="Product-img"></a> <a href="https://8mekb.bemobtrcks.com/go/d370498b-af01-4e76-91e6-3e60e08d5173"><img
+                src="https://d33wubrfki0l68.cloudfront.net/f4981ae7063dd63a36956d8258de0b9f7839361d/de9f6/images/3nhqxlolqzxj.gif" class="main-img mbt-1"
+                alt="Order-now"></a> </div>
+                <div class="flx jus-1 alg-cntr">
+                    <div class="clms-1">
+                        <h3>Milk Thistley</h3>
+                        <ul class="udc">
+                            <li>Potent fat-melting effect</li>
+                            <li>Supports liver health</li>
+                            <li>Promotes healthy blood sugar</li>
+                        </ul>
+                    </div>
+                    <div class="clms-1">
+                        <h3>Taraxum</h3>
+                        <ul class="udc">
+                            <li>Flushes out clogged fat</li>
+                            <li>Supports healthy cholesterol &amp; blood pressure</li>
+                            <li>Maintains healthy digestion</li>
+                        </ul>
+                    </div>
+                    <div class="clms-1">
+                        <h3>Panax Ginseng</h3>
+                        <ul class="udc">
+                            <li>Shrinks fat cells</li>
+                            <li>Boosts energy</li>
+                            <li>Potent aphrodisiac effect</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="flx jus-1 alg-cntr mbt-1">
+                    <div class="clms-1">
+                        <h3>Resveratrol</h3>
+                        <ul class="udc">
+                            <li>Reduces fat mass</li>
+                            <li>Supports a healthy heart and arteries</li>
+                            <li>Rejuvenates cells</li>
+                        </ul>
+                    </div>
+                    <div class="clms-1">
+                        <h3>Citrus Pectin</h3>
+                        <ul class="udc">
+                            <li>Reduces cravings</li>
+                            <li>Improve cognitive health</li>
+                            <li>Flush toxic metals from the body</li>
+                        </ul>
+                    </div>
+                    <div class="clms-1">
+                        <h3>ECGC</h3>
+                        <ul class="udc">
+                            <li>Promotes fat burning</li>
+                            <li>Powerful antioxidant</li>
+                            <li>Supports a healthy heart &amp; brain</li>
+                        </ul>
+                    </div>
+                </div>
+                <h4>+ A proprietary blend of 8 additional ingredients : Beet Root, Hibiscus, Strawberry Extract, Acai Extract, African Mango Extract, Black Currant Extract, Blueberry Powder.</h4>
+                <p>Ikaria Lean Belly Juice ingredients work in synergy with each other to support weight loss in individuals.</p>
+                <p>These ingredients have proven benefits for health, all users are expected to follow the dosage guidelines and use this product as per directions. Misusing or changing the dosage is not advised.</p>
+                <!---------benefit------>
+                <h2 class="mn-hed al-cnter">Ikaria Lean Belly Juice Benefits</h2>
+                <p>Ikaria Lean Belly Juice is the #1 natural supplement that burns excess stubborn fat fast easily and effectively. Ikaria Lean Belly Juice uses 100% natural and fantastic nutrients Ingredients to supports healthy weight loss and improve
+                    your overall health.</p>
+                <p>If you will start taking Ikaria Lean Belly Juice you\'ll see multiple benefits in your overall health.</p>
+                <ul class="udc">
+                    <li>100% Natural Formula</li>
+                    <li>No Stimulants</li>
+                    <li>Easy To Mix</li>
+                    <li>Non-GMO</li>
+                    <li>Vegetarian</li>
+                    <li>Non-Habit Forming</li>
+                </ul>
+               
+                    <br>
+                </p>
+                <ul class="udc">
+                </ul>
+                <!-------money----------->
+                <h2 class="mn-hed al-cnter">100% Satisfaction 180-Day Money Back Guarantee</h2>
+                <p>They are so confident you’ll enjoy life-changing results that they’re prepared to back up the Ikaria Lean Belly Juice with a 100% satisfaction guarantee for the next 180 days. If you are not absolutely astounded by how quickly your excess
+                    fat melt from your frame, and how much younger and healthier you feel day after day…</p>
+                <p>Then Company will only be too happy to refund you every cent. No questions asked. It’s time to get in the driver’s seat and be in control of your health again.</p>
+               
+                </p>
+                <h2 class="mn-hed al-cnter">Can I take the Ikaria Lean Belly Juice with other supplements?</h2>
+                <p>The Ikaria Lean Belly Juice should be compatible with other supplements without any problems or adverse effects. However, Company do not recommend you take Lean Belly Juice together with supplements containing similar ingredients.</p>
+                <h2 class="mn-hed al-cnter">How many bottles of the Ikaria Lean Belly Juice should I order?</h2>
+                <p>The Ikaria Lean Belly Juice’s ingredients are scientifically proven to have their greatest effect after 90 days or more.</p>
+                <p>This is why it makes a lot of sense for you to get started with the 3 or 6-month packages.</p>
+                <p>Sure, you can order a 30-day supply today - and you’ll be so convinced by how great you feel in a week or two that you’ll be likely to order more.</p>
+                <p>That\'s why taking advantage of Their 3 or 6-bottle deals below is the best choice for you.</p>
+               
+                <div class="mant-sec al-cnter mbt-1"> <a href="https://8mekb.bemobtrcks.com/go/d370498b-af01-4e76-91e6-3e60e08d5173"><img
+                src="https://d33wubrfki0l68.cloudfront.net/7b0e514e8c27bfe8cb4221bda2443ad05f360dfc/84699/images/fawo4pcm66wj.png" class="main-img-i"
+                alt="Product-img"></a> <a href="https://8mekb.bemobtrcks.com/go/d370498b-af01-4e76-91e6-3e60e08d5173"><img
+                src="https://d33wubrfki0l68.cloudfront.net/f4981ae7063dd63a36956d8258de0b9f7839361d/de9f6/images/3nhqxlolqzxj.gif" class="main-img"
+                alt="Order-now"></a><br>
+                    <h2 class="mn-hed al-cnter">100% SATISFACTION 180-DAY MONEYBACK GUARANTEE</h2>
+                    <a href="https://8mekb.bemobtrcks.com/go/d370498b-af01-4e76-91e6-3e60e08d5173"><img
+                src="https://d33wubrfki0l68.cloudfront.net/ba563c0d5c2298210c6963947b52249a4d2a0309/6373c/images/bmewrufsugzr.png" class="main-img-g mbt-1"
+                alt="Guarantee"></a> </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <center>
+        <style>
+            .embed-container {
+                position: relative;
+                padding-bottom: 56.25%;
+                height: 0;
+                overflow: hidden;
+                max-width: 80%;
+            }
+
+            .embed-container iframe,
+            .embed-container object,
+            .embed-container embed {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+            }
+        </style>
+       
+    </div>
+    <br>
+
+
+   
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END - Generated by ClassySchema.org -->
+
+
+    <div class="footr al-cnter " align="center">
+        <p>Copyright © Ikaria Lean Belly Juice 2022. All Rights Reserved</p>
+        <p class="foo-link"><a href="https://ikaria.netlify.app/">Home</a> | <a href=\'/privacy\' moz-do-not-send=\'true\'>Privacy Policy</a> | <a href=\'/terms\' moz-do-not-send=\'true\'>Terms &amp;
+          Conditions</a> | <a href=\'/contact\' moz-do-not-send=\'true\'>Contact Us</a></p>
+    </div>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-S9B5164ZX8"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag(\'js\', new Date());
+
+        gtag(\'config\', \'G-S9B5164ZX8\');
+    </script>
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org/",
+            "@type": "Product",
+            "name": "Ikaria lean belly juice",
+            "image": "https://d33wubrfki0l68.cloudfront.net/0d452551c4c421c3491489824e8f5db6b8006283/bbf86/images/ikaria-lean-official-where-can-i-buy-ikaria-lean-belly-juice-uk-australia-usa.png",
+            "brand": "Ikaria",
+            "description": "The Ikaria Lean Belly Juice is a potent blend of exotic nutrients specifically developed to rid your body of the newly discovered root cause of stubborn fat accumulation… destructive ceramide compounds.",
+            "aggregateRating": {
+                "@type": "AggregateRating",
+                "ratingValue": "4.2",
+                "ratingCount": "5",
+                "bestRating": "5",
+                "worstRating": "1"
+            }
+        }
+    </script>
+</body>
+
+</html>
+';
+?>
